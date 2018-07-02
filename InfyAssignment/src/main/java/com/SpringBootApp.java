9082ee1d38519d7ced6f6c1075f9420f0b3c23e9
Raut26 <40534253+Raut26@users.noreply.github.com>
2018-07-02 10:23:41 +0530

    Update SpringBootApp.java

--- InfyAssignment/src/main/java/com/SpringBootApp.java
+++ InfyAssignment/src/main/java/com/SpringBootApp.java
@@ -7,13 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootApp {
 	
    public static void main(String[] args) {
-	
-	   
-	   System.out.println("Main method start----------");
 	   
 	   SpringApplication.run(SpringBootApp.class, args);
-	   
-	   System.out.println("Main method end----------");
+	 
 	   
 }
 	
